e7e8ae501244d54b271e393472a2898e2327ebc0
Arup Kumar Behera <76170236+ArupGhoost@users.noreply.github.com>
2021-06-12 13:05:10 +0530

    Add files via upload

---
+++ MainReducer.js
@@ -0,0 +1,10 @@
+import changeTheNumber from './indexReducer';
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    changeTheNumber
+}
+)
+
+export default rootReducer;
